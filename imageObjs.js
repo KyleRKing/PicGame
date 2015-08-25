a8331967cc0b54841e83bb5c76207ac4fad2629e
Kyle King <kylerking@me.com>
2015-08-25 08:47:26 -0700

    add css and Objects in js

---
+++ imageObjs.js
@@ -0,0 +1,179 @@
+//Created objects for each image with answers and urls
+
+var image1 = {
+	imageNumber: "1",
+	name: "Liberty",
+	answers: ["Statue of Liberty", "StatueofLiberty", "Lady Liberty", "LadyLiberty"],
+	url: "https://upload.wikimedia.org/wikipedia/commons/d/d3/Statue_of_Liberty,_NY.jpg"
+}
+
+
+
+var image2 = {
+	imageNumber: "2",
+	name: "Queen",
+	answers: ["Queen Elizabeth", "QueenElizabeth", "Elizabeth", "Queen of England", "QueenofEngland"],
+	url: "https://upload.wikimedia.org/wikipedia/commons/5/50/Queen_Elizabeth_II_March_2015.jpg"
+}
+
+
+
+var image3 = {
+	imageNumber: "3",
+	name: "WhiteHouse",
+	answers: ["White House", "The White House", "WhiteHouse", "TheWhiteHouse"],
+	url: "http://media3.s-nbcnews.com/i/newscms/2014_29/566951/20140716-whitehouse-exterior-sl-1129_831e6c52ef7ae9024bed60b9dbb035df.jpg"
+}
+
+
+
+var image4 = {
+	imageNumber: "4",
+	name: "GoldenGate",
+	answers: ["Golden Gate", "GoldenGate", "Golden Gate Bridge", "GoldenGateBridge", "San Francisco Golden Gate Bridge", "SanFranciscoGoldenGateBridge"],
+	url: "http://www.wallcdn.com/wp-content/uploads/2015/04/San-Francisco-Golden-Gate-Bridge-High-Quality-Image-zyrig-Free1.jpeg"
+}
+
+
+
+var image5 = {
+	imageNumber: "5",
+	name: "OperaHouse",
+	answers: ["Sydney Opera House", "SydneyOperaHouse", "Sydney", "Opera House","OperaHouse"],
+	url: "http://www.hdwallpapersos.com/wp-content/uploads/2015/04/Sydney-Opera-House-Wallpaper.jpg"
+}
+
+
+
+var image6 = {
+	imageNumber: "6",
+	name: "CityHall",
+	answers: ["City Hall", "CityHall", "Los Angeles City Hall", "LosAngelesCityHall"],
+	url: "http://blogs.kcrw.com/whichwayla/wp-content/uploads/2013/05/DSC04176.jpg"
+}
+
+
+
+var image7 = {
+	imageNumber: "7",
+	name: "Washington",
+	answers: ["Washington", "George Washington", "GeorgeWashington", "President Washington", "PresidentWashington"],
+	url: "http://www.cottoneauctions.com/uploads/display_item/37/0305washdaylight.jpg"
+}
+
+
+
+var image8 = {
+	imageNumber: "8",
+	name: "SpaceNeedle",
+	answers: ["Space Needle", "SpaceNeedle", "Seattle Space Needle", "Seattle", "SeattleSpaceNeedle" ],
+	url: "https://upload.wikimedia.org/wikipedia/commons/6/60/Space_Needle_at_dusk_2011_-_02.jpg"
+}
+
+
+
+var image9 = {
+	imageNumber: "9",
+	name: "Eiffel",
+	answers: ["Eiffel", "Eiffel Tower"],
+	url: ""
+}
+
+
+
+var image10 = {
+	imageNumber: "10",
+	name: "Taxi",
+	answers: ["Taxi", "Taxi Cab", "TaxiCab", "Cab"],
+	url: "http://www.holiday-rentals.co.il/media/taxi2.jpg"
+}
+
+
+
+var image11 = {
+	imageNumber: "11",
+	name: "Denali",
+	answers: ["Mt. McKinley", "Mt.McKinley","MtMcKinley", "Mount McKinley", "Denali", "The Great One", "Denali National Park", "Mt. McKinley National Park"],
+	url: "http://media2.s-nbcnews.com/j/newscms/2015_24/1069696/150610-mckinley-10p_c5c1613760a6d4f8aebe16175f4c222d.nbcnews-ux-2880-1000.jpg"
+
+
+}
+
+var image12 = {
+	imageNumber: "12",
+	name: "Einstein",
+	answers: ["Einstein", "Albert Einstein", "AlbertEinstein"],
+	url: "https://upload.wikimedia.org/wikipedia/commons/3/3e/Einstein_1921_by_F_Schmutzer_-_restoration.jpg"
+}
+
+
+
+var image13 = {
+	imageNumber: "13",
+	name: "Kermit",
+	answers: ["Kermit", "Kermit the Frog", "KermittheFrog"],
+	url: "https://upload.wikimedia.org/wikipedia/en/6/62/Kermit_the_Frog.jpg"
+}
+
+
+
+var image14 = {
+	imageNumber: "14",
+	name: "Obama",
+	answers: ["Obama", "Barak Obama", "BarakObama", "President Obama", "PresidentObama", "The President"],
+	url: "http://images.christianpost.com/full/79853/president-barack-obama.jpg"
+}
+
+
+
+var image15 = {
+	imageNumber: "15",
+	name: "Lincoln",
+	answers: "Lincoln", "Abraham Lincoln", "AbrahamLincoln", "Abe", "Honest Abe", "HonestAbe"],
+	url: "http://begorok.com/file/abraham_lincoln_pictures_of_his_kids.jpg"
+}
+
+
+
+var image16 = {
+	imageNumber: "16",
+	name: "Moon",
+	answers: ["Moon", "The Moon"],
+	url: "http://www.torange.us/photo/26/16/round-moon-1387455646_89.jpg"
+}
+
+
+
+var image17 = {
+	imageNumber: "17",
+	name: "Austin",
+	answers: ["Austin Powers", "AustinPowers", "Mike Myers"],
+	url: "https://upload.wikimedia.org/wikipedia/en/4/42/Mike-Myers-Austin-Powers-1-.jpg"
+}
+
+
+
+var image18 = {
+	imageNumber: "18",
+	name: "Bart",
+	answers: ["Bart Simpson", "BartSimpson", "Bart"]
+	url: "http://www.allfreevectors.com/images/Free%20Vector%20Bart%20Simpson%2001%20The%20Simpsons1838.jpg"
+}
+
+
+
+var image19 = {
+	imageNumber: "19",
+	name: "Broccoli",
+	answers: ["Broccoli", "Brocoli", "Brocolli", "Brocali"]
+	url: "http://www.4us2be.com/wp-content/uploads/2010/03/Calcium-Rich-And-Dairy-Free-Broccoli.jpg"
+}
+
+
+
+var image20 = {
+	imageNumber: "20",
+	name: "Puffins",
+	answers: ["Puffin", "Puffins", "Pufins", "Pufin"]
+	url: "http://www.seaadventures.is/images/atlantic-puffin-seabird-blur-bg-u719.png"
+}
