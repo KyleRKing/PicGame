582ae4c3986991435e0503b1585fff1ab6c843d5
Kyle King <kylerking@me.com>
2015-08-26 10:20:37 -0700

    Solve Random Background Image

--- main.js
+++ main.js
@@ -6,20 +6,17 @@
 // need randomizer function to randomize the object/image selected
 
 
-// need randomizer function to select which box to fade randomily
 
-
-// need eventListener for player "A" on the #1 key
+// need eventListener for player "A" on the #1 key /// same things for player "B"
 //		function to pause the fading boxes
 //		function to make the "input box" active
 //		function to check the image object to see if answer is correct
 //		if correct, a function to ++ the score for player "A"
 //		if incorrect, a function to resume the fading windows
 
-// need eventListener for player "B" on the #2 key
-//		will pause the fading boxes
-//		make the "input box" active
-
+var imgCount = 20;
+var randomCount = [Math.floor((Math.random() * imagesArray.length)+ 1)];
+document.getElementById("container").style.backgroundImage = "url(" + imagesArray[randomCount].url + ")";
 
 
 document.getElementById("myButton").addEventListener("click", function(){
