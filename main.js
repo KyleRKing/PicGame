f9d71bcf3508a8cf939acae988c24ab631404166
Kyle King <kylerking@me.com>
2015-08-26 20:41:33 -0700

    Add lowercase eventlistener answercheck

--- main.js
+++ main.js
@@ -1,51 +1,129 @@
-//event lister on "start button" when pressed, begins game
-//		loads picture
-//		starts boxes fading
-
-
-// need randomizer function to randomize the object/image selected
-
-
-
-// need eventListener for player "A" on the #1 key /// same things for player "B"
-//		function to pause the fading boxes
-//		function to make the "input box" active
-//		function to check the image object to see if answer is correct
-//		if correct, a function to ++ the score for player "A"
-//		if incorrect, a function to resume the fading windows
 
 var imgCount = 20;
-
+var player1 = "Player 1";
+var player2 = "Player 2";
+var correct = null;
 
 function fadeBoxes() {
-	// if (boxes.className = "transparent")
 	var boxes = document.querySelectorAll(".box");
 	var randNum = Math.floor(Math.random() * ( boxes.length - 1 ) );
-	boxes[randNum].className += " transparent";
-		
+	var box = boxes[randNum];
+		console.log(box);
+	//need code if box already contains .class transparent...to skip over it
+	if (!box.classList.contains("transparent")) {
+		box.className += " transparent";
+	}
 }
 
 var interval;
 
-
+//THIS IS THE START LAUNCH LOGIC
 document.getElementById("myButton").addEventListener("click", start); 
-//pressing start should 
 function start() {
 	resetBoxes //removes the .class transparent from the white divs
+	//next two lines randomly selects the background image
 	var randomCount = Math.floor( Math.random() * ( imagesArray.length - 1 ) );
 	document.getElementById("container").style.backgroundImage = "url(" + imagesArray[randomCount].url + ")";
-
-	
-
 	interval = setInterval(fadeBoxes, 1000)
-	//and start the eventListener for the "Z" or "M" keys
-	//and start the var paused "true or false" logic
+	correct=imagesArray[randomCount].answers
+		console.log(correct);
+	//check buzzer
+	
 }
-//need to clean up so it only selects each box once
 
-resetBoxes = function() {
+
+resetBoxes = function() {  //functioin to do the remove process .class for transparent
+
 	$(".boxes").remove.class("transparent")
 }
+
+//score board arrays for each player
+
+//event listener for "Z" and "M" keys
+
+document.body.addEventListener("keydown", function(e) {
+	if (e.which==77){
+		clearInterval(interval);
+		getAnswer(player1)
+
+	}
+});
+
+
+
+document.body.addEventListener("keydown", function(e) {
+	if (e.which==90){
+		clearInterval(interval);
+		getAnswer(player2)
+
+	}
+});
+
+
+
+function getAnswer(player){
+	var answer = prompt("Please enter your answer").toLowerCase();
+		console.log(answer);
+	
+	var win = false; 
+		for (var i = 0; i <correct.length; i++){
+			var check=correct[i].toLowerCase();
+			if (answer == check){
+				win = true;
+			}
+		}
+
+		if (win){
+			console.log("you win");
+		}else{
+			console.log("you lose");
+		}
+
+}
+
+// $(document).ready(function () {
+// 	$('body').on('keydown keyup',function(e){
+// 	      if(e.which==77){
+// 	          console.log(e)
+// 	      }
+// 	    });
+// });
+// function checkBuzzer () {
+// 	if (buzzer1==true || buzzer2==true){
+// 		clearInterval(interval);
+// 		//toggle to buzzer pressed
+// 	}
+// }
+
+// function buzzer1 (true) {
+// 	window.prompt("sometext","Enter your Answer");
+// 	//compare CheckWin function to prompt function
+// 		//if(yes){
+// 			//increment player 1 score +10 points
+// 		}else{
+// 			//increment player 1 score -10 points
+// 		}
+// }
+
+// function buzzer2 (true) {
+// 	window.prompt("sometext","Enter your Answer");
+// 	//compare CheckWin function to prompt function
+// 		//if(yes){
+// 			//increment player 1 score +10 points
+// 		}else{
+// 			//increment player 1 score -10 points
+// 		}
+// }
+
+// function resetBuzzer() {
+// 	buzzer1=false;
+// 	buzzer2=false;
+// }
+
+
+
+
+
 
 
 
